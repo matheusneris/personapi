f0bfc057d78da261e2b6cab29124a5d554798575
matheusneris <matheusneris7@gmail.com>
2022-01-28 21:02:25 -0300

    Melhoria na funcionalidade de criacao de pessoa

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,23 +1,29 @@
 package one.digitalinnovation.personapi.service;
 
+import one.digitalinnovation.personapi.dto.mapper.PersonMapper;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPearson(Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPearson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Pessoa criada com ID " + savedPerson.getId())
